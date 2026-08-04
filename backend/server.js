fdfa030461b769adf5fcec376bb9b5b1a31aa021
Faizan Ali <alammdfaizan50@gmail.com>
2026-08-04 14:49:36 +0530

    feat: update CORS configuration to specify allowed origins for chatbot server

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+	cors({
+		origin: [
+			'http://localhost:5500',
+			'http://127.0.0.1:5500',
+			'https://geminiapichatbot.netlify.app/',
+		],
+	}),
+);
 app.use(express.json());
 
 const client = new OpenAI({
